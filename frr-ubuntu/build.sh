4372162926d4ab025d8f8bd7b4edc5c3fe1fb421
Ariel Weher <ariel@weher.net>
2024-11-07 18:19:20 -0300

    Comment out Docker push commands in build scripts for FRR images

--- frr-ubuntu/build.sh
+++ frr-ubuntu/build.sh
@@ -5,5 +5,5 @@ NOW=$(date +%Y%m%d%H%M%S)
 docker build --no-cache \
     -t $REGISTRY/public/$IMAGE:latest \
     -t $REGISTRY/public/$IMAGE:$NOW .
-docker push $REGISTRY/public/$IMAGE:latest
-docker push $REGISTRY/public/$IMAGE:$NOW
+#docker push $REGISTRY/public/$IMAGE:latest
+#docker push $REGISTRY/public/$IMAGE:$NOW
