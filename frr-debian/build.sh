21df74a7a0b31279922bfb367ba5adc1a08a4176
Ariel Weher <ariel@weher.net>
2024-11-07 18:14:51 -0300

    Add build scripts for FRR Docker images and update .gitignore

---
+++ frr-debian/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+REGISTRY=registry.apps.arreg.la
+IMAGE=frr-debian
+NOW=$(date +%Y%m%d%H%M%S)
+docker build --no-cache \
+    -t $REGISTRY/public/$IMAGE:latest \
+    -t $REGISTRY/public/$IMAGE:$NOW .
+docker push $REGISTRY/public/$IMAGE:latest
+docker push $REGISTRY/public/$IMAGE:$NOW
