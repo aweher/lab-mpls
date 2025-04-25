348386fb793560b11bda550935a3f62257db1558
Ariel S. Weher <ariel@weher.net>
2025-04-25 18:07:02 -0300

    Refactor start-influx.sh: move initial setup process to occur after InfluxDB availability check for improved flow

--- influxdb/start-influx.sh
+++ influxdb/start-influx.sh
@@ -11,25 +11,23 @@ INFLUXDB_TOKEN=localnetwork
 echo "[INFO] Iniciando influxd en background..."
 influxd &
 
-if [ ! -f /var/lib/influxdb2/setup.done ]; then
-  echo "[INFO] Realizando setup inicial..."
-
-  influx setup \
-    --bucket "$INFLUXDB_BUCKET" \
-    --org "$INFLUXDB_ORG" \
-    --username "$INFLUXDB_USERNAME" \
-    --password "$INFLUXDB_PASSWORD" \
-    --token "$INFLUXDB_TOKEN" \
-    --retention 30d \
-    --force
-
-  touch /var/lib/influxdb2/setup.done
-  echo "[INFO] InfluxDB configurado correctamente."
-fi
-
 echo "[INFO] Esperando que InfluxDB esté disponible..."
 until curl -s localhost:8086/health | grep -q '"status":"pass"'; do
   sleep 1
 done
+
+echo "[INFO] Realizando setup inicial..."
+
+influx setup \
+  --bucket "$INFLUXDB_BUCKET" \
+  --org "$INFLUXDB_ORG" \
+  --username "$INFLUXDB_USERNAME" \
+  --password "$INFLUXDB_PASSWORD" \
+  --token "$INFLUXDB_TOKEN" \
+  --retention 30d \
+  --force
+
+touch /var/lib/influxdb2/setup.done
+echo "[INFO] InfluxDB configurado correctamente."
 
 wait -n
